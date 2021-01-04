7cddb148530ed7056a14a027fbd5d92c82df654a
Camron Hughes <chughes314256@gmail.com>
2021-01-04 17:56:08 -0600

    Add files via upload

---
+++ src/bruce/effects/BRUCEEffects.java
@@ -0,0 +1,6 @@
+package bruce.effects;
+
+public class BRUCEEffects
+{
+
+}
---
+++ src/bruce/effects/BRUCEReverb.java
@@ -0,0 +1,17 @@
+package bruce.effects;
+import bruce.sound.*;
+
+public class BRUCEReverb extends BRUCEEffects
+{
+
+	BRUCEUtil byteUtil = new BRUCEUtil();
+	/**
+	 * 
+	 */
+	public void applyReverb(SoundSource currentSource) {
+		
+	}
+	
+	
+	
+}
